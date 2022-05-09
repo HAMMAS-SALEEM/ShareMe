10675568aa9dd85e25633a869e0bf5da21f98541
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-09 20:05:54 +0500

    :checkered_flag: Finalize Tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before(:example) { get '/users/show/posts' }
+    before(:example) { get '/users/1/posts' }
     it 'renders http status' do
       expect(response).to have_http_status :ok
     end
