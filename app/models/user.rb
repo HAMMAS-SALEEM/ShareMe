f6c9f9971fddda76490b5a6f995c13b6d18533f6
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-16 22:17:21 +0500

    :construction: Working on methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  def recent_users
+    users = User.all
+    users.each do |person|
+      "Username is #{person}"
+    end
+  end
 end
