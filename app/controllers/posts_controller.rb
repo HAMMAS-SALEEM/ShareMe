2c8430854c3fc343a6975b258a0f122aa2398a74
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-19 20:14:45 +0500

    :memo: added form for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,28 @@ class PostsController < ApplicationController
   end
 
   def new
-    post = Post.new
-    respond_to do |format|
-      format.html { render :new, locals: { post: } }
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(Title: post_params[:Title], Text: post_params[:Text])
+    if @post.save
+      @post.update_posts_counter
+      redirect_to current_user
+      flash[:notice] = 'You have successfully created a post'
+    else
+      render :new, alert: 'Error'
     end
   end
 
-  def create; end
-
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
   end
 end
