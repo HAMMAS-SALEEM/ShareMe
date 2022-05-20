9dff1bc7a358bd350a482830248e42d2f50338c1
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-20 15:44:38 +0500

    :rocket: Added unit tests for methods of model post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -39,13 +39,22 @@ RSpec.describe Post, type: :model do
     end
   end
 
-  context 'testing methods of user model' do
-    it 'should return recent users' do
-      # user = User.create(Name: 'Peter', Photo: '123.jpg', Bio: 'Developer from US', Posts_Counter: 0)
-      # post = Post.create(Title: 'Intro to JS', Text: 'Full Stack Development', CommentsCounter: 0, LikesCounter: 0,
-      #                    user_id: user.id)
-      # users = users.recent_posts
-      # expect(users.length).to be 1
+  context 'testing methods of post model' do
+    it 'should return recent comments' do
+      Comment.create(Text: 'Great Intro', user_id: user.id, post_id: subject.id)
+      Comment.create(Text: 'It has everything', user_id: user.id, post_id: subject.id)
+      Comment.create(Text: 'Do you have an academy?', user_id: user.id, post_id: subject.id)
+      Comment.create(Text: "Who's your teacher", user_id: user.id, post_id: subject.id)
+      Comment.create(Text: 'Would you like to give your facebook', user_id: user.id, post_id: subject.id)
+      Comment.create(Text: 'Do you have these projects on github', user_id: user.id, post_id: subject.id)
+      comments = subject.recent_comments
+      expect(comments.length).to be 5
+    end
+
+    it 'should increment the Post counter in respectice user' do
+      subject.update_posts_counter
+      user_posts = subject.user.Posts_Counter
+      expect(user_posts).to be 1
     end
   end
 end
