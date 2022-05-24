cf270674df62a768c0d2a2477d2676f5fab9003a
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-24 20:38:16 +0500

    :rotating_light: working on devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :likes
   has_many :comments
