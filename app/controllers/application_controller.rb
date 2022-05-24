f17b1d256e1f70bef4901783ddc6f70978705de2
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-24 18:59:14 +0500

    :lock: getting out of problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
 
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
