29d41b72f649b4873129f1ce3fdee4584061d3da
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-24 18:32:42 +0500

    :construction: work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
+
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -6,10 +8,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:Name, :Bio, :Photo, :password, :password_confirmation, :email)
+    end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+      u.permit(:Name, :Bio, :Photo, :password, :password_confirmation, :email, :current_password)
     end
   end
 end
-
