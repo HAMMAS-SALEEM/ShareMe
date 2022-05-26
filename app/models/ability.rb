2fceeca897a960f97639938859c4420a6459498a
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-26 08:39:34 +0500

    :ambulance: fixed linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,16 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.role == 'admin'
-      can :manage, :all
+    can :manage, :all if user.role == 'admin'
+    can :destroy, Post do |post|
+      post.try(:user) == user
     end
-      can :destroy, Post do |post|
-        post.try(:user) == user
-      end
-      can :create, Comment
-      can :destroy, Comment do |comment|
-        comment.try(:user) == user
-      end
+    can :create, Comment
+    can :destroy, Comment do |comment|
+      comment.try(:user) == user
+    end
     can :read, :all
   end
 end
