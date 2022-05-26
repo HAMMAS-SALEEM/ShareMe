750058ff5f4f51189b1e42eef4b3af2cd435d567
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-27 02:09:44 +0500

    :recycle: added tests for user index page

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'login page', type: :feature do
   before(:each) do
-    user = User.create(Name: 'example', Posts_Counter: 0, email: 'example@email.com', password: 'hammas')
+    User.create(Name: 'example', Posts_Counter: 0, email: 'example@email.com', password: 'hammas')
     visit user_session_path
   end
   context 'should render login page elements' do
@@ -32,11 +32,11 @@ RSpec.describe 'login page', type: :feature do
       expect(page).to have_content 'Invalid Email or password.'
     end
     it 'should redirect to users page after logging in with right entries' do
-        visit new_user_session_path
-        fill_in 'Email', with: 'example@email.com'
-        fill_in 'Password', with: 'hammas'
-        click_button 'Log in'
-        expect(page).to have_content 'You have to confirm your email address before continuing.'
+      visit new_user_session_path
+      fill_in 'Email', with: 'example@email.com'
+      fill_in 'Password', with: 'hammas'
+      click_button 'Log in'
+      expect(page).to have_content 'You have to confirm your email address before continuing.'
     end
   end
 end
--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe 'User index page', type: :feature do
   before(:each) do
     @user = User.create(Name: 'Hammas', Photo: 'img.jpg', Bio: 'Developer', email: 'test@email.com',
                         password: 'password', confirmed_at: Time.now)
-    @post = Post.create(user: @user, Title: 'My title', Text: 'My text')
-    @like = Like.create(user: @user, post_id: @post.id)
+    @user.confirm
     visit new_user_session_path
     fill_in 'Email', with: 'test@email.com'
     fill_in 'Password', with: 'password'
@@ -17,11 +16,15 @@ RSpec.describe 'User index page', type: :feature do
   end
 
   it 'shows the users profile picture when clicked' do
-    expect(page.find('img')['src']).to have_content 'img.jpg'
+    expect(page).to have_css("img[src*='img.jpg']")
+  end
+
+  it 'shows Post Counter of user' do
+    expect(page).to have_content('Number of Posts: 0')
   end
 
   it 'redirects to that users show page when a user clicked' do
     click_link('Hammas', match: :prefer_exact)
-    expect(page).to have_current_path(user_path(@user.id.to_s))
+    expect(page).to have_content('Bio')
   end
 end
