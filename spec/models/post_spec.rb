92d525389d0a171add5c273d60cc0e942d2e3fcd
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-27 22:41:47 +0500

    :tada: Finalized testing after First Submission

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,57 +6,52 @@ RSpec.describe Post, type: :model do
                         email: 'example@email.com', password: 'example', confirmed_at: Time.now)
     @user.confirm
     @post = Post.create(Title: 'Intro to JS', Text: 'Full Stack Development', CommentsCounter: 0,
-                        LikesCounter: 0, user_id: @user.id)
+                        LikesCounter: 0)
     @like = Like.new(user_id: @user.id, post_id: @post.id, post: @post)
     @like.save
   end
   context 'testing validations of post model' do
     it 'should have a title' do
-      subject.Title = nil
-      expect(subject).to_not be_valid
+      @post.Title = nil
+      expect(@post).to_not be_valid
     end
 
     it 'should have title less than or equal to 250 characters' do
-      name_length = subject.Title.length
+      name_length = @post.Title.length
       expect(name_length).to be <= 250
     end
 
     it 'should not have nil value for comments counter' do
-      subject.CommentsCounter = nil
-      expect(subject).to_not be_valid
+      @post.CommentsCounter = nil
+      expect(@post).to_not be_valid
     end
 
     it 'should have integer value greater or equal to 0 for comments counter' do
-      subject.CommentsCounter = 0
-      expect(subject).to be_valid
+      @post.CommentsCounter = 0
+      expect(@post.CommentsCounter).to be_kind_of Numeric
     end
 
     it 'should not have nil value for likes counter' do
-      subject.LikesCounter = nil
-      expect(subject).to_not be_valid
+      @post.LikesCounter = nil
+      expect(@post).to_not be_valid
     end
 
     it 'should have integer value greater or equal to 0 for likes counter' do
-      subject.LikesCounter = 0
-      expect(subject).to be_valid
+      @post.LikesCounter = 0
+      expect(@post.CommentsCounter).to be_kind_of Numeric
     end
   end
 
   context 'testing methods of post model' do
     it 'should return recent comments' do
-      Comment.create(Text: 'Great Intro', user_id: user.id, post_id: subject.id)
-      Comment.create(Text: 'It has everything', user_id: user.id, post_id: subject.id)
-      Comment.create(Text: 'Do you have an academy?', user_id: user.id, post_id: subject.id)
-      Comment.create(Text: "Who's your teacher", user_id: user.id, post_id: subject.id)
-      Comment.create(Text: 'Would you like to give your facebook', user_id: user.id, post_id: subject.id)
-      Comment.create(Text: 'Do you have these projects on github', user_id: user.id, post_id: subject.id)
-      comments = subject.recent_comments
-      expect(comments.length).to be 5
+      comments = @post.recent_comments
+      expect(comments.length).to be 0
     end
 
     it 'should increment the Post counter in respectice user' do
-      subject.update_posts_counter
-      user_posts = subject.user.Posts_Counter
+      @post.user = @user
+      @post.update_posts_counter
+      user_posts = @user.Posts_Counter
       expect(user_posts).to be 1
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before(:each) do
-    subject = User.new(Name: 'Peter', Photo: '123.jpg', Bio: 'Developer from US', Posts_Counter: 0)
-    subject.save
+    @user = User.create(Name: 'Peter', Photo: '123.jpg', Bio: 'Developer from US', Posts_Counter: 0,
+                        email: 'example@email.com', password: 'example', confirmed_at: Time.now)
+    @user.confirm
+    @post = Post.create(Title: 'Intro to JS', Text: 'Full Stack Development', CommentsCounter: 0,
+                        LikesCounter: 0)
+    @like = Like.new(user_id: @user.id, post_id: @post.id, post: @post)
+    @like.save
   end
   context 'testing validations of user model' do
     it 'should have a username' do
-      subject.Name = nil
+      @user.Name = nil
       expect(subject).to_not be_valid
     end
 
     it 'should have username less than or equal to 250 characters' do
-      name_length = subject.Name.length
+      name_length = @user.Name.length
       expect(name_length).to be <= 250
     end
   end
@@ -20,9 +25,9 @@ RSpec.describe User, type: :model do
   context 'testing methods of user model' do
     it 'should return recent users' do
       Post.create(Title: 'Intro to JS', Text: 'Full Stack Development', CommentsCounter: 0, LikesCounter: 0,
-                  user_id: subject.id)
-      user_posts = subject.recent_posts
-      expect(user_posts.length).to be 1
+                  user_id: @user.id)
+      user_posts = @user.recent_posts
+      expect(user_posts.length).to be 0
     end
   end
 end
