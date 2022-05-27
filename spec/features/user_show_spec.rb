212b2f0b237bbb99423e3e7bc5661dcdbc6db559
Gegardus <stgegard@gmail.com>
2022-05-27 14:01:42 +0400

    Add post show test

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'User show page', type: :feature do
   before(:each) do
-    @user = User.create(id: 17, Name: 'Hammas', Photo: 'img.jpg', Bio: 'Developer', email: 'test@email.com',
+    @user = User.create(Name: 'Hammas', Photo: 'img.jpg', Bio: 'Developer', email: 'test@email.com',
                         password: 'password', confirmed_at: Time.now)
     @user.confirm
-    @post = Post.create(id: 1, user: @user, Title: '2', Text: 'Full Stack Development')
+    @post = Post.create(user: @user, Title: '2', Text: 'Full Stack Development')
     visit new_user_session_path
     fill_in 'Email', with: 'test@email.com'
     fill_in 'Password', with: 'password'
@@ -33,9 +33,10 @@ RSpec.describe 'User show page', type: :feature do
       click_link 'See all posts'
       expect(page).to have_button('Pagination')
     end
-    # it 'should return individual post' do
-    #   click_button 'See all posts'
-    #   expect(current_path).to eq user_url id: @user.id
-    # end
+    it 'should return individual post' do
+      click_button 'See all posts'
+      click_link 'Intro to JS'
+      expect(page).to have_content('Intro to JS')
+    end
   end
 end
